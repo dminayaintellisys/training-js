565fad2ddf8c4fdad215c30387c7f0ef8039d36d
David Minaya <dminaya@intellisys.com.do>
2019-09-26 10:48:35 -0400

    Update bmi-comparator.js

--- challenge-1/bmi-comparator.js
+++ challenge-1/bmi-comparator.js
@@ -1,6 +1,7 @@
 class Person {
 
-    constructor(mass, height) {
+    constructor(name, mass, height) {
+        this.name = name;
         this._mass = mass;
         this._height = height;
     }
@@ -10,9 +11,13 @@ class Person {
     }
 }
 
-const mark = new Person(78, 1.69);
-const john = new Person(92, 1.95);
+const mark = new Person('Mark', 78, 1.69);
+const john = new Person('John', 92, 1.95);
 
 const isMarkBmiHigher = mark.bodyMassIndex > john.bodyMassIndex;
 
-console.log(isMarkBmiHigher ? mark.bodyMassIndex : john.bodyMassIndex);
+if (isMarkBmiHigher) {
+    console.log(`${mark.name} is the winner with a bmi of: ${mark.bodyMassIndex}`);
+} else {
+    console.log(`${john.name} is the winner with a bmi of: ${john.bodyMassIndex}`);
+}
