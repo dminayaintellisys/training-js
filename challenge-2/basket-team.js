2a6ad144c9e2a4218fea081c338fbafc0638bb19
dminayaintellisys <53197027+dminayaintellisys@users.noreply.github.com>
2019-09-30 13:42:25 -0400

    Update basket-team.js

--- challenge-2/basket-team.js
+++ challenge-2/basket-team.js
@@ -1,18 +1,35 @@
 class BasketTeam {
 
+    /**
+     * @constructor
+     * @param {nameTeam} name - name of the team of basketball
+     */
     constructor(nameTeam) {
         this._nameTeam = nameTeam;
         this._scores = [];
     }
 
+    /**
+     * @return {string} the basket's team name
+     */
     get nameTeam() {
         return this._nameTeam;
     }
 
+    /**
+     * Add one or several scores to the team
+     *
+     * @param {number[]} score - the score(s) of the team 
+     */
     addScore(...score) {
         this._scores.push(...score);
     }
 
+    /**
+     * Calculate the team's average score
+     *
+     * @return {number} team's average score
+     */
     get average() {
         const sumOfAllScores = this._scores.reduce((summedScores, score) => summedScores + score);
         const average = sumOfAllScores / this._scores.length;
