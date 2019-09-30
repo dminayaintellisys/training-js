b7610fa3de7905e31f2fbe6a5c507c35468d74c0
dminayaintellisys <53197027+dminayaintellisys@users.noreply.github.com>
2019-09-30 14:14:02 -0400

    Update tips-average-calculator.js

--- challenge-5/tips-average-calculator.js
+++ challenge-5/tips-average-calculator.js
@@ -1,3 +1,10 @@
+/**
+ * Calculate the tips's average
+ * 
+ * @param {number[]} tips - tips
+ *
+ * @return {number} tips' average
+ */
 const calculateTipsAverage = function(tips) {
     return tips.reduce((tipsAdded, tip) => tipsAdded + tip) / tips.length;
 };
