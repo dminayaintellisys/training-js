30120dcae9db3e97b14316e1e124add662b27444
dminayaintellisys <53197027+dminayaintellisys@users.noreply.github.com>
2019-09-30 13:49:31 -0400

    Added comments

--- challenge-4/bmi-comparator.js
+++ challenge-4/bmi-comparator.js
@@ -1,5 +1,11 @@
 class Person {
 
+    /**
+     * @constructor
+     * @param {string} name - name of the person
+     * @param {number} mass - mass of the person in kg
+     * @param {number} height - height of the person in ft
+     */
     constructor(name, mass, height) {
         this._name = name;
         this._mass = mass;
@@ -7,10 +13,18 @@ class Person {
         this._bodyMassIndex = 0;
     }
 
+    /**
+     * return {string} name of the person
+     */
     get name() {
         return this._name;
     }
 
+    /**
+     * Calculate the body mass index of the person
+     *
+     * @return {number} the body mass index of the person
+     */
     get bodyMassIndex() {
         this._bodyMassIndex = this._mass / (this._height * this._height);
         return this._bodyMassIndex;
