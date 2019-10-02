6dd199e6ec3e63a96f3ff34012b91ae450bddc4a
dminayaintellisys <53197027+dminayaintellisys@users.noreply.github.com>
2019-10-02 08:52:17 -0400

    Update average-score.js

--- challenge-2/average-score.js
+++ challenge-2/average-score.js
@@ -8,14 +8,23 @@ johnTeam.addScore(89, 120, 103);
 mikeTeam.addScore(116, 94, 123);
 maryTeam.addScore(97, 134, 105);
 
-let winner = {};
-
+// Is john's team winner
 if (johnTeam.average > mikeTeam.average && johnTeam.average > maryTeam.average) {
-    winner = johnTeam;
+    
+    console.log(`${johnTeam.nameTeam} is the winner: ${johnTeam.average}`);
+    
+// Is mike's team winner
 } else if (mikeTeam.average > johnTeam.average && mikeTeam.average > maryTeam.average) {
-    winner = mikeTeam;
+    
+    console.log(`${mikeTeam.nameTeam} is the winner: ${mikeTeam.average}`);
+    
+// Is mary's team winner
+} else if (maryTeam.average > johnTeam.average && maryTeam.average > mikeTeam.average) {
+    
+    console.log(`${maryTeam.nameTeam} is the winner: ${maryTeam.average}`);
+    
+// is a draw
 } else {
-    winner = maryTeam;
+    
+    console.log(`All teams are draws`);
 }
-
-console.log(`${winner.nameTeam} is the winner: ${winner.average}`);
